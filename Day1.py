5c99b1005a29dcaa256554fead7c84b09be884b6
Gabriel Gilberto Aguiar de Oliveira <gabrielgilberto98@hotmail.com>
2025-03-22 14:34:01 -0300

    First commit - 14:34 Brazil - 03/22/2025

---
+++ Day1.py
@@ -0,0 +1,7 @@
+# 1. Write a program that prints "Hello, World!" to the console
+# 2. Add single-line and multi-line comments to your code
+
+# Isso é um comentário de uma linha
+# Estou escrevendo um programa que imprime "Hello, World!" na tela
+
+print("Hello, World!")
---
+++ Day2.py
@@ -0,0 +1,26 @@
+"""
+Create the following variables of different types and print them.
+    name: String
+    age: Integer
+    height: Floating-point number
+    is_student: Boolean
+    email: None (representing a null value)
+    siblings: List
+    address: Dictionary containing street and city
+"""
+
+name = "Gabriel Gilberto" 
+age = 25
+height = 1.74
+is_student = True
+email = "gabrielgilberto@fisica.ufc.br"
+siblings = ["Manel", "André"]
+address = {"street": "Rua W14", "city": "Caucaia"}
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+print(email)
+print(siblings)
+print(address)
