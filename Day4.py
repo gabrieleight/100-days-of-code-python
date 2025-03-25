0e9ac80b4f54fb0a2d77ad9632a19143104fdce8
Gabriel Gilberto Aguiar de Oliveira <gabrielgilberto98@hotmail.com>
2025-03-24 21:29:07 -0300

    First commit - 21:29 Brazil - 03/24/2025

---
+++ Day4.py
@@ -0,0 +1,59 @@
+"""
+Arithmetic Operators
+Write a program to perform the following arithmetic operations using two numbers:
+Addition (+)
+Subtraction (-)
+Multiplication (*)
+Division (/)
+Floor Division (//)
+Modulus (%)
+Exponentiation (**)
+
+
+Relational Operators
+Write a program to compare two numbers using the following operators:
+Equal to (==)
+Not equal to (!=)
+Greater than (>)
+Less than (<)
+Greater than or equal to (>=)
+Less than or equal to (<=)
+
+
+Logical Operators
+Write a program that evaluates the following between 2 booleans(True or False):
+Logical AND (and)
+Logical OR (or)
+Logical NOT (not)
+"""
+
+#Operações Aritméticas
+numero1 = 10
+numero2 = 9
+
+print("Operações Aritméticas")
+print(f"Adição: {numero1 + numero2}")
+print(f"Subtração: {numero1 - numero2}")
+print(f"Multiplicação: {numero1 * numero2}")
+print(f"Divisão: {numero1 / numero2}")
+print(f"Divisão Inteira: {numero1 // numero2}")
+print(f"Módulo: {numero1 % numero2}")
+print(f"Exponenciação: {numero1 ** numero2}")
+print()
+
+#Operadores Relacionais
+print("Operadores Relacionais")
+print(f"Igual: {numero1 == numero2}")
+print(f"Diferente: {numero1 != numero2}")
+print(f"Maior: {numero1 > numero2}")
+print(f"Menor: {numero1 < numero2}")
+print(f"Maior ou Igual: {numero1 >= numero2}")
+print(f"Menor ou Igual: {numero1 <= numero2}")
+print ()
+
+#Operadores Lógicos
+print("Operadores Lógicos")
+print(f"AND: {True and False}")
+print(f"OR: {True or False}")
+print(f"NOT: {not True}")
+print()
